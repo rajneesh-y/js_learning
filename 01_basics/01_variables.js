0dd2430830a24718a7a60eb9568a43d116925d09
rajneesh <rajneeshy568@gmail.com>
2023-10-01 23:56:50 +0530

    variable basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const acountId = 3456
+let accountEmail = "rajneesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Lucknow"
+
+console.log(acountId, accountEmail, accountPassword, accountCity)
+
+console.table([acountId, accountEmail, accountPassword, accountCity])
