18728f7902527563e07658157039a8b6138de0d6
Rajneesh Yadav <111866507+rajneesh-y@users.noreply.github.com>
2023-10-03 19:53:56 +0000

    comparision with null and undefined

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+console.log("--------------------------------")
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log("-------------------")
+
+console.log("2"===2)
