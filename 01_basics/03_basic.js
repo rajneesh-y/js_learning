0411cfe6180c320149a1631855af4df1e417ee3d
Rajneesh Yadav <111866507+rajneesh-y@users.noreply.github.com>
2023-10-09 02:29:18 +0000

    Data Types

---
+++ 01_basics/03_basic.js
@@ -0,0 +1,7 @@
+// primitive datatype
+// 1.Numbear 2.String 3.Boolean 4.Undefined 4.null
+// Non- Primitive Datatypes
+// 1.Object 2.Array 3.Fuction
+
+// Stack(All Primitive Data Type)---->It is like call by value like in c its only give copy
+//Heap(All Non Primitive Data Types)---> It is like call by reference in c it give reference of the Object 
